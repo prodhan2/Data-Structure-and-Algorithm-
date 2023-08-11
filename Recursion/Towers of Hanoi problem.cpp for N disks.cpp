c3a801cf88c351ebcb05b74901d1cf8516dddb6e
Sujan prodhan <113437122+prodhan2@users.noreply.github.com>
2023-08-11 15:46:29 +0600

    Update Towers of Hanoi problem.cpp for N disks.cpp

--- Recursion/Towers of Hanoi problem.cpp for N disks.cpp
+++ Recursion/Towers of Hanoi problem.cpp for N disks.cpp
@@ -1,23 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void towerOFHanoi(int n,char source , char auxi ,char disti) {
-    if(n==1)
-    {
-        cout<<"move disk 1 from "<< source <<" to "<< disti <<endl;
-        return;
-    }
-    towerOFHanoi(n-1,source,disti,auxi);
-      cout<<"move disk "<< n<<" from "<< source <<" to "<< disti <<endl;
-      towerOFHanoi(n-1,auxi,source,disti);
+ void tower(int n,char beg,char aux,char disti)
+ {
+     if(n<=0)
+        cout<<"invalid type"<<endl;
+     else if (n==1)
+          cout <<"dises moive from "<< beg <<"   TO  "<<disti<<endl;
+     else
+     {
+     tower(n-1,beg,disti,aux);
+     tower(1,beg,aux,disti);
+     tower(n-1,aux,beg,disti);
+
+     }
 }
 
-int main()
-{
-  int n;
-  cout<<"HOW MANY DISK"<<endl;
-   cin>>n;
 
-towerOFHanoi(n,'a','b','c');
+ int main()
+{
+    int n;
+    cout<<"Number of dise"<<endl;
+    cin>>n;
+
+    tower(n,'A','B','C');
 
 }
