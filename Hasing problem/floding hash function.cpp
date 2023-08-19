c93aab6d27f9264536607479a13d84ff8ec7e5c0
Sujan prodhan <113437122+prodhan2@users.noreply.github.com>
2023-08-20 01:05:48 +0600

    Update floding hash function.cpp

--- Hasing problem/floding hash function.cpp
+++ Hasing problem/floding hash function.cpp
@@ -1,3 +1,30 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int flodingHashFunction(int data,int size )
+{
+int sum =0;
+    while(data >0)
+    {
+        sum += data%1000;
+        data = data /1000;
+    }
+    return sum %size ;
+}
+int main()
+{
+  cout<< flodingHashFunction(123456789,17) <<endl;
+}
+
+
+
+
+orrrrrrrrrrrrrrrrrrrrrr
+------------------------
+
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int flodinghashfunction(int n,int size)
@@ -20,3 +47,5 @@ int main()
    int index =  flodinghashfunction(123456789,size);
    cout <<index <<endl;
 }
+
+
