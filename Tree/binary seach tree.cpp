aee2fc89e579c0359335f479564c8975ae1c4f4f
Sujan prodhan <113437122+prodhan2@users.noreply.github.com>
2023-08-24 23:04:36 +0600

    Update binary seach tree.cpp

--- Tree/binary seach tree.cpp
+++ Tree/binary seach tree.cpp
@@ -1,50 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define mx 1000
-int bst[mx];
-void Insert(int val)
-{
+
+#define max 1000
+vector<int> v(max, -1);
+
+void insert(int m) {
     int index = 0;
-    while(bst[index]!=-mx)
-    {
-        if(bst[index]==val)
-        {
-            cout<<val <<" is found at "<<index<<endl;
+    while (v[index] != -1) {
+        if (v[index] == m) {
+            cout << "Item is already exist at index " << index << endl;
             return;
-        }
-        else if(val>bst[index])
-        {
-            index = 2*index + 2;
-        }
-        else
-        {
-            index = 2*index + 1;
+        } else if (m > v[index]) {
+            index = 2 * index + 1;  // Move to the left child
+        } else {
+            index = 2 * index + 2;  // Move to the right child
         }
     }
-    bst[index] = val;
+    v[index] = m;  // Insert the value at the empty slot
 }
-int main()
-{
-    fill(bst,bst+mx,-mx);
-    cout<<"Number of element : "<<endl;
-    int n;
-    cin>>n;
-    for(int i=0;i<n;i++)
-    {
-        int item;
-        cin>>item;
-        Insert(item);
 
+int main() {
+    cout << "How many numbers you want to insert: " << endl;
+    int n;
+    cin >> n;
+    
+    for (int i = 0; i < n; i++) {
+        int m;
+        cin >> m;
+        insert(m);  // Insert each number into the vector
     }
-    for(int i=0;i<mx;i++)
-    {
-        if(bst[i]!=-mx)
-        {
-            cout<<bst[i]<<" ";
+
+    cout << "Elements in the vector: ";
+    for (int i = 0; i < max; i++) {
+        if (v[i] != -1) {
+            cout << v[i] << " ";  // Print the non-empty elements of the vector
         }
     }
-    cout<<endl;
-    Insert(12);
+    cout << endl;
+    
     return 0;
-
 }
